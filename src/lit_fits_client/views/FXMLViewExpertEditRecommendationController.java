557c5ab107721dbf319316c971eebe0b09ff5bd3
Ander <Ander@HZ341341.TartangaLH.eus>
2020-01-16 14:14:00 +0100

    FXMLcontrollers doing...

---
+++ src/lit_fits_client/views/FXMLViewExpertEditRecommendationController.java
@@ -0,0 +1,258 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lit_fits_client.views;
+
+import java.util.logging.Logger;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+import lit_fits_client.entities.Color;
+import lit_fits_client.entities.FashionExpert;
+import lit_fits_client.entities.Material;
+
+/**
+ *
+ * @author Ander
+ */
+public class FXMLViewExpertEditRecommendationController extends FXMLDocumentController {
+    @FXML
+    private Button btnSave;
+    @FXML
+    private Button btnCancel;
+    @FXML
+    private TableView<Color> tableColor;
+    @FXML
+    private TableColumn<Color, String> columnNameColor;
+    @FXML
+    private TableColumn<Color, Boolean> columnSelectMaterial;
+    @FXML
+    private TableView<Material> tableMaterial;
+    @FXML
+    private TableColumn<Material, String> columnNameMaterial;
+    @FXML
+    private TableColumn<Material, Boolean> columnSelectMMaterial;
+
+    private ObservableList<Color> colorList;
+    private ObservableList<Material> materialList;
+    
+    private Stage stage;
+    private Stage stageMainMenu;
+    
+    private FashionExpert expert;
+    private static final Logger LOG = Logger.getLogger(FXMLViewExpertEditRecommendationController.class.getName());
+
+    /**
+     * @return the btnSave
+     */
+    public Button getBtnSave() {
+        return btnSave;
+    }
+
+    /**
+     * @param btnSave the btnSave to set
+     */
+    public void setBtnSave(Button btnSave) {
+        this.btnSave = btnSave;
+    }
+
+    /**
+     * @return the btnCancel
+     */
+    public Button getBtnCancel() {
+        return btnCancel;
+    }
+
+    /**
+     * @param btnCancel the btnCancel to set
+     */
+    public void setBtnCancel(Button btnCancel) {
+        this.btnCancel = btnCancel;
+    }
+
+    /**
+     * @return the tableColor
+     */
+    public TableView<Color> getTableColor() {
+        return tableColor;
+    }
+
+    /**
+     * @param tableColor the tableColor to set
+     */
+    public void setTableColor(TableView<Color> tableColor) {
+        this.tableColor = tableColor;
+    }
+
+    /**
+     * @return the columnNameColor
+     */
+    public TableColumn<Color, String> getColumnNameColor() {
+        return columnNameColor;
+    }
+
+    /**
+     * @param columnNameColor the columnNameColor to set
+     */
+    public void setColumnNameColor(TableColumn<Color, String> columnNameColor) {
+        this.columnNameColor = columnNameColor;
+    }
+
+    /**
+     * @return the columnSelectMaterial
+     */
+    public TableColumn<Color, Boolean> getColumnSelectMaterial() {
+        return columnSelectMaterial;
+    }
+
+    /**
+     * @param columnSelectMaterial the columnSelectMaterial to set
+     */
+    public void setColumnSelectMaterial(TableColumn<Color, Boolean> columnSelectMaterial) {
+        this.columnSelectMaterial = columnSelectMaterial;
+    }
+
+    /**
+     * @return the tableMaterial
+     */
+    public TableView<Material> getTableMaterial() {
+        return tableMaterial;
+    }
+
+    /**
+     * @param tableMaterial the tableMaterial to set
+     */
+    public void setTableMaterial(TableView<Material> tableMaterial) {
+        this.tableMaterial = tableMaterial;
+    }
+
+    /**
+     * @return the columnNameMaterial
+     */
+    public TableColumn<Material, String> getColumnNameMaterial() {
+        return columnNameMaterial;
+    }
+
+    /**
+     * @param columnNameMaterial the columnNameMaterial to set
+     */
+    public void setColumnNameMaterial(TableColumn<Material, String> columnNameMaterial) {
+        this.columnNameMaterial = columnNameMaterial;
+    }
+
+    /**
+     * @return the columnSelectMMaterial
+     */
+    public TableColumn<Material, Boolean> getColumnSelectMMaterial() {
+        return columnSelectMMaterial;
+    }
+
+    /**
+     * @param columnSelectMMaterial the columnSelectMMaterial to set
+     */
+    public void setColumnSelectMMaterial(TableColumn<Material, Boolean> columnSelectMMaterial) {
+        this.columnSelectMMaterial = columnSelectMMaterial;
+    }
+
+    /**
+     * @return the colorList
+     */
+    public ObservableList<Color> getColorList() {
+        return colorList;
+    }
+
+    /**
+     * @param colorList the colorList to set
+     */
+    public void setColorList(ObservableList<Color> colorList) {
+        this.colorList = colorList;
+    }
+
+    /**
+     * @return the materialList
+     */
+    public ObservableList<Material> getMaterialList() {
+        return materialList;
+    }
+
+    /**
+     * @param materialList the materialList to set
+     */
+    public void setMaterialList(ObservableList<Material> materialList) {
+        this.materialList = materialList;
+    }
+
+    /**
+     * @return the stage
+     */
+    public Stage getStage() {
+        return stage;
+    }
+
+    /**
+     * @param stage the stage to set
+     */
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    /**
+     * @return the stageMainMenu
+     */
+    public Stage getStageMainMenu() {
+        return stageMainMenu;
+    }
+
+    /**
+     * @param stageMainMenu the stageMainMenu to set
+     */
+    public void setStageMainMenu(Stage stageMainMenu) {
+        this.stageMainMenu = stageMainMenu;
+    }
+
+    /**
+     * @return the expert
+     */
+    public FashionExpert getExpert() {
+        return expert;
+    }
+
+    /**
+     * @param expert the expert to set
+     */
+    public void setExpert(FashionExpert expert) {
+        this.expert = expert;
+    }
+
+    void initStage(String value, Stage stageMainProgram, Parent root, String uri) {
+        try{
+            this.uri = uri;
+            this.stage = stageMainProgram;
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("Edit recommendations");
+            stage.setMinWidth(1400);
+            stage.setMinHeight(800);
+            stage.show();
+            setStylesheet(scene, theme);
+            setElements();
+            stage.setOnCloseRequest(this::onClosing);
+        }catch(Exception e){
+            createExceptionDialog(e);
+            LOG.severe(e.getMessage());
+        }
+    }
+
+    private void setElements() {
+        
+    }
+
+    
+}
