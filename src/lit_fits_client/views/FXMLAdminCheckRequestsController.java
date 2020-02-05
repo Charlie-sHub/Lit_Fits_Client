b89dde6268e66dbd197761a6b07b6889f37d2957
Asier <@.>
2020-02-05 11:01:56 +0100

    AdminCheckRequests corrections. "Create test garments" option added

--- src/lit_fits_client/views/FXMLAdminCheckRequestsController.java
+++ src/lit_fits_client/views/FXMLAdminCheckRequestsController.java
@@ -23,7 +23,10 @@ import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.core.GenericType;
 import lit_fits_client.RESTClients.ClientFactory;
 import lit_fits_client.RESTClients.GarmentClient;
+import lit_fits_client.entities.BodyPart;
 import lit_fits_client.entities.Garment;
+import lit_fits_client.entities.GarmentType;
+import lit_fits_client.entities.Mood;
 import lit_fits_client.entities.User;
 import lit_fits_client.views.themes.Theme;
 
@@ -68,7 +71,7 @@ public class FXMLAdminCheckRequestsController extends FXMLDocumentController{
     // ----------------- Garments Table ----------------------
     
     @FXML
-    private TableView<Garment> garmentsTable;
+    private TableView<Garment> tableViewGarment;
     @FXML
     private TableColumn<Garment, String> barcodeColumn;
     @FXML
@@ -87,6 +90,17 @@ public class FXMLAdminCheckRequestsController extends FXMLDocumentController{
     private TableColumn<Garment, Boolean> promotionRequestColumn;
     
     private ObservableList<Garment> garmentList;
+    
+    // ---------------- Modificaciones DIN -------------------
+        
+        private ObservableList<Garment> requestedGarmentList;
+        private ObservableList<Garment> promotedGarmentList;
+    
+        @FXML
+        private MenuItem menuItemCreateTestGarments;
+        
+        private String filter = "";
+    //--------------------------------------------------------
     
     /**
      * Returns the admin that is currently logged.
@@ -176,12 +190,39 @@ public class FXMLAdminCheckRequestsController extends FXMLDocumentController{
      * 
      * @throws ClientErrorException 
      */
+    /*
     private void fillTable() throws ClientErrorException {
         GarmentClient garmentClient = ClientFactory.getGarmentClient(uri);
         
         garmentList = FXCollections.observableArrayList(garmentClient.findGarmentAll(new GenericType<List<Garment>>(){}));
         
         garmentsTable.setItems(garmentList);
+    }
+    */
+    private void fillTable() {
+        
+        try {
+            switch(this.filter) {
+
+                case "Promoted":
+                    this.tableViewGarment.setItems(FXCollections.observableArrayList(promotedGarmentList));
+                    break;
+
+                case "Promotion request":
+                    this.tableViewGarment.setItems(FXCollections.observableArrayList(requestedGarmentList));
+                    break;
+
+                case "Not promoted/requested":
+                    this.tableViewGarment.setItems(FXCollections.observableArrayList(garmentList));
+                    break;
+
+                default:
+                    break;
+            }
+        } catch (NullPointerException ex) {
+            
+            this.createDialog("There are no garments.");
+        }
     }
     
     /**
@@ -212,9 +253,8 @@ public class FXMLAdminCheckRequestsController extends FXMLDocumentController{
         this.fillChoiceBoxTheme();
         this.setChoiceBoxEntities();
         
-        // The method to fill the table. It does not work
-        //this.setColumnFactories();
-        //this.fillTable();
+        this.setColumnFactories();
+        this.fillTable();
     }
     
     /**
@@ -225,9 +265,9 @@ public class FXMLAdminCheckRequestsController extends FXMLDocumentController{
         
         filterByList.add("Promoted");
         filterByList.add("Promotion request");
+        filterByList.add("Not promoted/requested");
         
         choiceBoxFilterBy.setItems(FXCollections.observableArrayList(filterByList));
-        choiceBoxFilterBy.setOnAction(this::onFilterChosen);
     }
     
     /**
@@ -237,21 +277,9 @@ public class FXMLAdminCheckRequestsController extends FXMLDocumentController{
      */
     private void onFilterChosen (Event event) {
         
-        String selected = this.choiceBoxFilterBy.getSelectionModel().getSelectedItem().toString();
+        this.filter = this.choiceBoxFilterBy.getSelectionModel().getSelectedItem().toString();
         
-       switch (selected) {
-           case "Promoted":
-               // Fill the table with only the promoted garments.
-            break;
-               
-           case "Promotion request":
-               // Fill the table with only the garments that request a promotion.
-            break;
-               
-           default:
-               
-            break;
-        }
+       this.fillTable();
     }
     
     /**
@@ -264,6 +292,8 @@ public class FXMLAdminCheckRequestsController extends FXMLDocumentController{
         menuFile.setMnemonicParsing(true);
         menuItemBack.setText("_Back");
         menuItemBack.setMnemonicParsing(true);
+        menuItemCreateTestGarments.setText("Create _test garments");
+        menuItemCreateTestGarments.setMnemonicParsing(true);
         menuEdit.setText("_Edit");
         menuEdit.setMnemonicParsing(true);
         menuItemPromote.setText("_Promote");
@@ -293,6 +323,7 @@ public class FXMLAdminCheckRequestsController extends FXMLDocumentController{
         choiceTheme.setOnAction(this::onThemeChosen);
         
         menuItemBack.setOnAction(this::onBtnBackPress);
+        menuItemCreateTestGarments.setOnAction(this::createTestGarments);
         menuItemPromote.setOnAction(this::onMenuItemPromotePress);
         menuItemDeletePromotion.setOnAction(this::onMenuItemDeletePromotionPress);
         menuItemCancelRequest.setOnAction(this::onMenuItemCancelRequestPress);
@@ -300,6 +331,8 @@ public class FXMLAdminCheckRequestsController extends FXMLDocumentController{
         btnBack.setOnAction(this::onBtnBackPress);
         
         choiceTheme.setOnAction(this::onThemeChosen);
+        
+        choiceBoxFilterBy.setOnAction(this::onFilterChosen);
     }
     
     /**
@@ -317,6 +350,7 @@ public class FXMLAdminCheckRequestsController extends FXMLDocumentController{
      * 
      * @param event The action event of the view.
      */
+    /*
     private void onMenuItemPromotePress(ActionEvent event) {
         Garment promoteGarment = garmentsTable.getSelectionModel().getSelectedItem();
         
@@ -328,12 +362,37 @@ public class FXMLAdminCheckRequestsController extends FXMLDocumentController{
         garmentClient.close();
         this.fillTable();
     }
+    */
+    private void onMenuItemPromotePress (ActionEvent event) {
+        
+        try {
+            if (this.filter == "Promotion request") {
+                Garment promotedGarment = tableViewGarment.getSelectionModel().getSelectedItem();
+
+                this.requestedGarmentList.remove(promotedGarment);
+
+                promotedGarment.setPromoted(true);
+                promotedGarment.setPromotionRequest(false);
+
+                this.promotedGarmentList.add(promotedGarment);
+
+                this.fillTable();
+
+            } else {
+                this.createDialog("This action is only available for \"Promotion request\" garments.");
+
+            }
+        } catch (NullPointerException ex) {
+            this.createDialog("Nothing is selected.");
+        }
+    }
     
     /**
      * The event that will happen when the <i>Delete promotion</i> menu item is pressed.
      * 
      * @param event The action event of the view.
      */
+    /*
     private void onMenuItemDeletePromotionPress(ActionEvent event) {
         Garment deletePromoteGarment = garmentsTable.getSelectionModel().getSelectedItem();
         
@@ -344,12 +403,34 @@ public class FXMLAdminCheckRequestsController extends FXMLDocumentController{
         garmentClient.close();
         this.fillTable();
     }
+    */
+    private void onMenuItemDeletePromotionPress(ActionEvent event) {
+        
+        try {
+            if (filter == "Promoted") {
+                Garment deletePromotionGarment = tableViewGarment.getSelectionModel().getSelectedItem();
+
+                this.promotedGarmentList.remove(deletePromotionGarment);
+                deletePromotionGarment.setPromoted(false);
+
+                this.garmentList.add(deletePromotionGarment);
+
+                this.fillTable();
+            }else {
+                this.createDialog("This action is only available for \"Promoted\" garments.");
+            }
+            
+        } catch (NullPointerException ex) {
+            this.createDialog("Nothing is selected.");
+        }
+    }
     
     /**
      * The event that will happen when the <i>Cancel request</i> menu item is pressed.
      * 
      * @param event The action event of the view.
      */
+    /*
     private void onMenuItemCancelRequestPress(ActionEvent event) {
         Garment cancelPromoteGarment = garmentsTable.getSelectionModel().getSelectedItem();
         
@@ -358,6 +439,202 @@ public class FXMLAdminCheckRequestsController extends FXMLDocumentController{
         GarmentClient garmentClient = ClientFactory.getGarmentClient(uri);
         garmentClient.editGarment(cancelPromoteGarment);
         garmentClient.close();
+        this.fillTable();
+    }
+    */
+    private void onMenuItemCancelRequestPress(ActionEvent event) {
+        
+        try {
+            if (this.filter == "Promotion request") {
+                Garment cancelPromoteGarment = tableViewGarment.getSelectionModel().getSelectedItem();
+
+                this.requestedGarmentList.remove(cancelPromoteGarment);
+
+                cancelPromoteGarment.setPromotionRequest(false);
+
+                this.garmentList.add(cancelPromoteGarment);
+            
+                this.fillTable();
+                
+            } else {
+                this.createDialog("This action is only available for \"Promotion request\" garments.");
+            }
+        } catch (NullPointerException ex) {
+            this.createDialog("Nothing is selected.");
+        }
+    }
+    
+    /**
+     * This method creates garments to test the app. Is a modification 
+     * to avoid calling the server.
+     */
+    private void createTestGarments (ActionEvent event) {
+        
+        if (this.requestedGarmentList != null){
+            this.requestedGarmentList.clear();
+        } else {
+            this.requestedGarmentList = FXCollections.observableArrayList(new ArrayList<Garment>());
+        }
+        
+        if (this.promotedGarmentList != null){
+            this.promotedGarmentList.clear();
+        } else {
+            this.promotedGarmentList = FXCollections.observableArrayList(new ArrayList<Garment>());
+        }
+        
+        if (this.garmentList != null){
+            this.garmentList.clear();
+        } else {
+            this.garmentList = FXCollections.observableArrayList(new ArrayList<Garment>());
+        }
+        
+        for (int i = 0; i<=4; i++) {
+            Garment requestedGarment = new Garment();
+            
+            requestedGarment.setBarcode(String.valueOf(1001*i));
+            requestedGarment.setMood(Mood.FORMAL);
+            requestedGarment.setDesigner("DIN - Requested garments");
+            
+            switch (i) {
+                case 0:
+                    requestedGarment.setBodyPart(BodyPart.TOP);
+                    break;                    
+                case 1:
+                    requestedGarment.setBodyPart(BodyPart.BOTTOM);
+                    break;                    
+                case 2:
+                    requestedGarment.setBodyPart(BodyPart.SHOE);
+                    break;                    
+                case 3:
+                    requestedGarment.setBodyPart(BodyPart.SHOE);
+                    break;                    
+                case 4:
+                    requestedGarment.setBodyPart(BodyPart.OTHER);
+                    break;                    
+            }
+            
+            switch (i) {
+                case 0:
+                    requestedGarment.setGarmentType(GarmentType.SHIRT);
+                    break;
+                case 1:
+                    requestedGarment.setGarmentType(GarmentType.PANTS);
+                    break;
+                case 2:
+                    requestedGarment.setGarmentType(GarmentType.BOOTS);
+                    break;
+                case 3:
+                    requestedGarment.setGarmentType(GarmentType.SNEAKERS);
+                    break;
+                case 4:
+                    requestedGarment.setGarmentType(GarmentType.HAT);
+                    break;    
+            }
+            requestedGarment.setAvailable(i%2 == 0);
+            requestedGarment.setPromotionRequest(true);
+            requestedGarment.setPromoted(false);
+            
+            this.requestedGarmentList.add(requestedGarment);
+        }
+        
+        for (int i = 0; i<=4; i++) {
+            Garment promotedGarment = new Garment();
+            
+            promotedGarment.setBarcode(String.valueOf(11*i));
+            promotedGarment.setMood(Mood.FORMAL);
+            promotedGarment.setDesigner("DIN - Promoted garments");
+            
+            switch (i) {
+                case 0:
+                    promotedGarment.setBodyPart(BodyPart.TOP);
+                    break;                    
+                case 1:
+                    promotedGarment.setBodyPart(BodyPart.BOTTOM);
+                    break;                    
+                case 2:
+                    promotedGarment.setBodyPart(BodyPart.SHOE);
+                    break;                    
+                case 3:
+                    promotedGarment.setBodyPart(BodyPart.SHOE);
+                    break;                    
+                case 4:
+                    promotedGarment.setBodyPart(BodyPart.OTHER);
+                    break;                    
+            }
+            
+            switch (i) {
+                case 0:
+                    promotedGarment.setGarmentType(GarmentType.SHIRT);
+                    break;
+                case 1:
+                    promotedGarment.setGarmentType(GarmentType.PANTS);
+                    break;
+                case 2:
+                    promotedGarment.setGarmentType(GarmentType.BOOTS);
+                    break;
+                case 3:
+                    promotedGarment.setGarmentType(GarmentType.SNEAKERS);
+                    break;
+                case 4:
+                    promotedGarment.setGarmentType(GarmentType.HAT);
+                    break;    
+            }
+            promotedGarment.setAvailable(i%2 == 0);
+            promotedGarment.setPromotionRequest(false);
+            promotedGarment.setPromoted(true);
+            
+            this.promotedGarmentList.add(promotedGarment);
+        }
+        
+        for (int i = 0; i<=4; i++) {
+            Garment garment = new Garment();
+            
+            garment.setBarcode(String.valueOf(110*i));
+            garment.setMood(Mood.FORMAL);
+            garment.setDesigner("DIN - All garments");
+
+            switch (i) {
+                case 0:
+                    garment.setBodyPart(BodyPart.TOP);
+                    break;                    
+                case 1:
+                    garment.setBodyPart(BodyPart.BOTTOM);
+                    break;                    
+                case 2:
+                    garment.setBodyPart(BodyPart.SHOE);
+                    break;                    
+                case 3:
+                    garment.setBodyPart(BodyPart.SHOE);
+                    break;                    
+                case 4:
+                    garment.setBodyPart(BodyPart.OTHER);
+                    break;                    
+            }
+            
+            switch (i) {
+                case 0:
+                    garment.setGarmentType(GarmentType.SHIRT);
+                    break;
+                case 1:
+                    garment.setGarmentType(GarmentType.PANTS);
+                    break;
+                case 2:
+                    garment.setGarmentType(GarmentType.BOOTS);
+                    break;
+                case 3:
+                    garment.setGarmentType(GarmentType.SNEAKERS);
+                    break;
+                case 4:
+                    garment.setGarmentType(GarmentType.HAT);
+                    break;
+            }
+            garment.setAvailable(i%2 == 0);
+            garment.setPromotionRequest(false);
+            garment.setPromoted(false);
+            
+            this.garmentList.add(garment);
+        }
+        
         this.fillTable();
     }
 }
